cf1a88467b3f79cba763ceb7e4f9ab1be1f61c24
yusuf <ybasori@gmail.com>
2023-01-19 17:43:25 +0700

    setup redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/lib/persistStore";
+import { store } from "./store";
 import ProductRoadmap from "./components/pages/ProductRoadmap/ProductRoadmap";
 
 function App() {
-  return <ProductRoadmap />;
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistStore(store)}>
+        <ProductRoadmap />
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/store.ts
@@ -0,0 +1,24 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  whitelist: ["auth"],
+  storage: storage,
+};
+
+const reducer = combineReducers({});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
